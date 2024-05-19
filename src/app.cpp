471cc8a9309b9ef4aa2346cd86daa02200c8706e
stadt77@mail.ru <stadt77@mail.ru>
2024-05-19 17:19:51 +0300

    Setup

--- src/app.cpp
+++ src/app.cpp
@@ -1,23 +1,23 @@
-﻿// CMakeProject1.cpp
-
-#include "app.h"
+﻿#include <iostream>
 #include <gtkmm.h>
+#include "app.h"
 
-class MyWindow : public Gtk::Window
-{
-public:
-  MyWindow();
-};
-
-MyWindow::MyWindow()
-{
-  set_title("Basic application");
-  set_default_size(200, 200);
+MyWindow::MyWindow() {
+	set_title("File clusterization master");
+	set_default_size(700, 500);
+	startButton.set_label("Begin Algorithm");
+	//startButton.set_margin_top(440); startButton.set_margin_bottom(10);
+	startButton.set_margin_start(300); startButton.set_margin_end(300);
+	startButton.signal_clicked().connect(sigc::mem_fun(*this, &MyWindow::OnStartClick));
+	beginText.set_text("This programm allows to automatically sort and cluster large quantities of files.\nBefore starting the algorithm, choose required settings.");
+	beginText.set_margin_top(10); beginText.set_margin_bottom(10);
+	beginText.set_margin_start(10); beginText.set_margin_end(100);
+	grid.attach(beginText, 0, 0);
+	grid.attach(startButton, 0, 4);
+	set_child(grid);
 }
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
   auto app = Gtk::Application::create("org.gtkmm.examples.base");
-
   return app->make_window_and_run<MyWindow>(argc, argv);
 }
--- src/app.h
+++ src/app.h
@@ -1,4 +1,18 @@
-﻿// CMakeProject1.h
 #pragma once
 
-#include <iostream>
+class MyWindow : public Gtk::Window {
+public:
+	MyWindow();
+	Gtk::Button startButton;
+	Gtk::Label beginText;
+	//Gtk::Box hbox;
+	Gtk::Grid grid;
+
+	void OnStartClick() {
+		if (startButton.get_label() == "Begin Algorithm") {
+			startButton.set_label("End Algorithm");
+		} else {
+			startButton.set_label("Begin Algorithm");
+		}
+	}
+};
